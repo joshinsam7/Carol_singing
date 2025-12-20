f30e570ac4096a69acf5c2d30c9714546bab3e62
Joshin Sam <joshinsam381@gmail.com>
2025-12-20 10:32:06 -0600

    Sienna data

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -4,8 +4,8 @@ import React, {useState} from "react";
 import "./header.css";
 import logo from "../assets/marthoma-seeklogo.png";
 export function Header({toggleInfo}) {
-  const [day, setDay] = useState(7);
-  const [location, setLocation] = useState("North Shore West");
+  const [day, setDay] = useState(8);
+  const [location, setLocation] = useState("Sienna");
   const [isMobileMenu, setIsMobileMenu] = useState(false);
 
   const handleHamburgerClick = () => {
